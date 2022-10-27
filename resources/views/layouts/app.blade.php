c7b406f063cc6b08284a8686b2134e0e6d190cb2
Mohamed benzarti <m.benzarti.1996@gmail.com>
2022-10-27 21:11:12 +0100

    reglage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -273,6 +273,7 @@
                                         <li class="nav-item">
                                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                         </li>
+                                        
                                     @endif
                                 @else
                                 <li class="nav-item">
