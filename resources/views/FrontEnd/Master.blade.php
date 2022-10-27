f46bb3d7a885f2e16ad87138931da9b652deb009
mounaayed <mouna.ayed@esprit.tn>
2022-10-27 16:49:58 +0200

    event

--- resources/views/FrontEnd/Master.blade.php
+++ resources/views/FrontEnd/Master.blade.php
@@ -262,6 +262,9 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('event.index') }}">Event</a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('Cyclicte.index') }}">Cyclicte</a>
+                                </li>
                                     <li class="nav-item dropdown">
                                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                             {{ Auth::user()->name }}
